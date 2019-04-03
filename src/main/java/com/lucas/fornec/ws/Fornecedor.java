036835f6e0ff7c9d1555b6f15cc275ef873d7377
Lucas P <lucas_wuthang@hotmail.com>
2019-04-02 22:32:59 -0300

    Ajustes no código

--- src/main/java/com/lucas/fornec/ws/Fornecedor.java
+++ src/main/java/com/lucas/fornec/ws/Fornecedor.java
@@ -15,34 +15,6 @@ public class Fornecedor {
 		this.id = id;
 	}
 	
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + id;
-		return result;
-	}
-	
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Fornecedor other = (Fornecedor) obj;
-		if (id != other.id)
-			return false;
-		return true;
-	}
-	
-	@Override
-	public String toString() {
-		return "Fornecedor [id=" + id + ", nome=" + nome + ", email=" + email + ", comentario=" + comentario + ", cnpj="
-				+ cnpj + "]";
-	}
-	
 	public String getNome() {
 		return nome;
 	}
